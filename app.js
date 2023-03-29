f42b8917040a1bd5531de9356089b56e24327838
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-03-29 16:46:22 +0200

    add package

--- app.js
+++ app.js
@@ -48,8 +48,11 @@ const apiKey = '0b699565b0417f81c60a035ad7b3c655'
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let name = input.value;
+    console.log(name);
+
     fetchFilm(name);
 })
+
 async function fetchFilm(name) {
     try {
         const response = await fetch("https://api.themoviedb.org/3/search/movie?api_key=" + apiKey +"&query=" + name)
@@ -60,12 +63,13 @@ async function fetchFilm(name) {
         // https://image.tmdb.org/t/p/w200${movie.poster_path}
         results.innerHTML = Object.values(data.results).map((movie, index) => `
             <div class="cards">
-                <img id='' src="${movie.poster_path != null ? 'https://image.tmdb.org/t/p/w200'+movie.poster_path : 'img/sans-affiche.png'}" alt="">
+                <img src="${movie.poster_path != null ? 'https://image.tmdb.org/t/p/w200'+movie.poster_path : 'img/sans-affiche.png'}" alt="">
                 <div>
-                    <h2><i>Titre</i> : ${movie.title}</h2>
-                    <h3><i>Résumé</i> : ${movie.overview}</h3>
+                    <h3><i>Titre</i> : ${movie.title}</h3>
+                    <h5>V.O. : ${movie.original_language}</h5>
+                    <p><i>Résumé</i> : ${movie.overview}</p>
                     <h4><i>Date de sortie</i> : ${movie.release_date}</h4>
-                    <h3><i>Note</i> : ${movie.vote_average} / 10 </h3>
+                    <h4><i>Note</i> : ${movie.vote_average} / 10 </h4>
                     <button class='popMovie' id='${index}'>Show Details</button>
                 </div>
             </div>
@@ -81,9 +85,6 @@ async function fetchFilm(name) {
 ///////////////////////////////////////////////////////MODAL///////////////////////////////////////////////////////
 // Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-//var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -115,15 +116,15 @@ function fillPop() {
 
 function displayModal(key) {
     console.log(movies[key].titre);
-        modal.style.display = "block";
-        popTitre.innerText = movies[key].title;
-        popOriginal.innerText = movies[key].original_title;
-        popDate.innerText = movies[key].release_date;
-        popOverview.innerText = movies[key].overview;
-        //popDesc.innerText = movies[key].description;
-        popLogo.src = 'https://image.tmdb.org/t/p/w500'+movies[key].poster_path;
+    modal.style.display = "block";
+    popTitre.innerText = movies[key].title;
+    popOriginal.innerText = '('+movies[key].original_title+')';
+    popDate.innerText = movies[key].release_date;
+    popOverview.innerText = movies[key].overview;
+    //popDesc.innerText = movies[key].description;
+    popLogo.src = 'https://image.tmdb.org/t/p/w500'+movies[key].poster_path;
         
-        //console.log("currentModalIndex : " + currentModalIndex);
+    //console.log("currentModalIndex : " + currentModalIndex);
 }
 
 // When the user clicks on <span> (x), close the modal
